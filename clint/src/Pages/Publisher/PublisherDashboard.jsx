ef01fb7698eec1b7bb79f4a560b301757a45a250
MdRoki2020 <mroki815@gmail.com>
2023-01-14 17:45:54 +0600

    added income graph

--- clint/src/Pages/Publisher/PublisherDashboard.jsx
+++ clint/src/Pages/Publisher/PublisherDashboard.jsx
@@ -4,6 +4,53 @@ import { FaBuysellads } from "react-icons/fa";
 import { SiHandshake,SiAnalogue } from "react-icons/si";
 import ReactPaginate from 'react-paginate'
 import { GiReturnArrow } from "react-icons/gi";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import '../../Assets/style/adminDashboard.css'
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const PublisherDashboard = () => {
   return (
@@ -44,6 +91,22 @@ const PublisherDashboard = () => {
           </div>
         </div>
 
+
+        <div className='row'>
+          <div className='col-md-6'>
+
+          </div>
+          <div className='col-md-6'>
+          <div className='card categoriesLavel my-3 shadow'>
+            <h5>Income Rate</h5>
+            <ResponsiveContainer width="100%" aspect={3}>
+              <LineChart width={300} height={100} data={data}>
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+              </LineChart>
+            </ResponsiveContainer>
+        </div>
+          </div>
+        </div>
 
       </div>
     </Fragment>
